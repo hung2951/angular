265d5be6447c161ab21fc43ec32aeb488b6b8517
van hung <hungtv290501@gmail.com>
2022-05-13 08:37:43 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  name = 'hungtv2951';
+  class = 'we16301';
+  students = [
+    {
+      name: 'hung1',
+      id: 'PH1',
+      status: 0
+    },
+    {
+      name: 'hung2',
+      id: 'PH2',
+      status: 0
+    },
+    {
+      name: 'hung3',
+      id: 'PH3',
+      status: 1
+    }
+  ];
+
+  LMHT = [
+    {
+      name: 'Eᴢreal',
+      dame: 500,
+      price: 6500,
+      avatar: 'https://camnang24h.net/wp-content/uploads/2019/04/hinh-nen-tuong-Ezreal-trong-lien-minh-huyen-thoai-6.jpg'
+    },
+    {
+      name: 'Draᴠen',
+      dame: 900,
+      price: 2000,
+      avatar: 'https://camnang24h.net/wp-content/uploads/2019/04/hinh-nen-tuong-Ezreal-trong-lien-minh-huyen-thoai-6.jpg'
+    },
+    {
+      name: 'Eᴢreal',
+      dame: 500,
+      price: 10000,
+      avatar: 'https://camnang24h.net/wp-content/uploads/2019/04/hinh-nen-tuong-Ezreal-trong-lien-minh-huyen-thoai-6.jpg'
+    }
+
+  ];
+  studentName = 'Trương Văn Hùng'
+  studentMSV = 'PH12345'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NameComponent } from './name/name.component';
+import { IdentityComponent } from './identity/identity.component';
+import { DameComponent } from './dame/dame.component';
+import { AvatarComponent } from './avatar/avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NameComponent,
+    IdentityComponent,
+    DameComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
